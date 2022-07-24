b002b111948147c2d3ae1f18f0f078dd85d953f8
Max Peterson <maxpeterson00@gmail.com>
2022-07-24 12:07:04 -0700

    i3: initial config

---
+++ i3/lock.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+BLANK='#00000000'
+CLEAR='#ffffff22'
+DEFAULT='#5e81accc'
+TEXT='#81a1c1ee'
+WRONG='#880000bb'
+VERIFYING='#bb00bbbb'
+
+i3lock \
+--insidever-color=$CLEAR     \
+--ringver-color=$VERIFYING   \
+\
+--insidewrong-color=$CLEAR   \
+--ringwrong-color=$WRONG     \
+\
+--inside-color=$BLANK        \
+--ring-color=$DEFAULT        \
+--line-color=$BLANK          \
+--separator-color=$DEFAULT   \
+\
+--verif-color=$TEXT          \
+--wrong-color=$TEXT          \
+--time-color=$TEXT           \
+--date-color=$TEXT           \
+--layout-color=$TEXT         \
+--keyhl-color=$WRONG         \
+--bshl-color=$WRONG          \
+\
+--screen 1                   \
+--blur 5                     \
+--clock                      \
+--indicator                  \
+--time-str="%_I:%M %p %Z"    \
+--date-str="%m/%d/%Y"        \
+\
+--radius=180                 \
+--date-size=16               \
